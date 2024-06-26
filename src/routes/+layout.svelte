1c287b4a5febac13b3098cb0b66babc2aa0ba2db
dinosaw44 <173938953+dinosaw44@users.noreply.github.com>
2024-06-26 18:18:20 +0100

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang=ts>
+    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome"
+    import { faGithub as githubIcon } from "@fortawesome/free-brands-svg-icons"
+    
+    import "../style.scss"
+</script>
+
+<style lang=scss>
+    header {
+        padding: 3ch;
+    }
+
+    nav {
+        display: flex;
+        justify-content: right;
+
+        a {
+            color: inherit;
+            text-decoration: none;
+        }
+    }
+</style>
+
+<header>
+    <nav style:font-size="1.75rem">
+        <a href="https://github.com/dinosaw44" title="Github Repo"><FontAwesomeIcon icon={githubIcon}/></a>
+    </nav>
+</header>
+
+<slot />
